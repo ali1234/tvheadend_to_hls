b0cdfd578349627786775775d3f339a9ea1bc8a3
Alistair Buxton <a.j.buxton@gmail.com>
2024-07-19 02:47:10 +0100

    Dynamic pipeline

--- src/tvhtohls/__main__.py
+++ src/tvhtohls/__main__.py
@@ -1,33 +1,27 @@
 #!/usr/bin/env python3
+
 import pathlib
+import shlex
 
 import uvicorn
 import os
 from fastapi import FastAPI, Response
 from fastapi.staticfiles import StaticFiles
-import requests
-from requests.auth import HTTPDigestAuth
 import time
-import sched
-import json
-import html
 import subprocess
-import threading
-import re
+
+import sys
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst, GObject
 
 
 #Read settings
 config={}
-config["tvheadend_ip"]="192.168.5.5"
-config["tvheadend_port"]="9981"
-config["tvheadend_user"]="user"
-config["tvheadend_pass"]="pass"
-config["local_port"]=8888
 config["hls_local_path"]="/tmp/tvhtohls/hls"
 config["hls_http_path"]="/hls/"
 config["static_local_path"]=pathlib.Path(__file__).parent / 'static'
-config["static_http_path"]="/static/"
-config["sort"]="name" # or "number"
+config["static_http_path"]="/"
 
 for setting in config.keys():
     if setting in os.environ:
@@ -38,273 +32,76 @@ if not os.path.isdir(config["hls_local_path"]):
     exit()
 
 
-tvh_base_url="http://"+config["tvheadend_ip"]+":"+config["tvheadend_port"]+"/"
-tvh_base_url_auth="http://"+config["tvheadend_user"]+":"+config["tvheadend_pass"]+"@"+config["tvheadend_ip"]+":"+config["tvheadend_port"]+"/"
 
-def tvheadend_get_raw(typ):
-    url=tvh_base_url+"api/raw/export?class="+typ
-    return tvheadend_get(url)
+class Streamer(FastAPI):
+    def __init__(self):
+        super().__init__()
+        self.pipeline = Gst.Pipeline.new("pipeline")
 
-def tvheadend_get(url):
-    req=requests.get(url, auth=HTTPDigestAuth(config["tvheadend_user"], config["tvheadend_pass"]))
-    req.encoding="UTF-8"
+        self.source = Gst.ElementFactory.make("videotestsrc", "source")
+        self.source.set_property("is-live", True)
+        self.overlay = Gst.ElementFactory.make("textoverlay", "overlay")
+        self.overlay.set_property("text", "TEST")
+        self.overlay.set_property("valignment", "center")
+        self.overlay.set_property("font-desc", "sans normal 80")
+        self.scaler = Gst.ElementFactory.make("videoconvertscale", "scaler")
+        caps = Gst.Caps.from_string("video/x-raw, width=480, height=270, format=I420")
+        self.filter = Gst.ElementFactory.make("capsfilter", "filter")
+        self.filter.set_property("caps", caps)
+        self.encoder = Gst.ElementFactory.make("x264enc", "encoder")
+        self.parser = Gst.ElementFactory.make("h264parse", "parser")
+        self.sink = Gst.ElementFactory.make(f"hlssink2", "hls")
+        self.sink.set_property("max-files", 5)
+        self.sink.set_property("send-keyframe-requests", True)
+        self.sink.set_property("target-duration", 2)
+        self.sink.set_property("location", config['hls_local_path'] + '/segment%05d.ts')
+        self.sink.set_property("playlist-location", config["hls_local_path"] + "/stream.m3u8")
 
-    if req.status_code != 200:
-        print("Couldn't get service list. Maybe user has insufficient rights. Code: ", req.status_code)
-        exit()
+        self.pipeline.add(self.source)
+        self.pipeline.add(self.overlay)
+        self.pipeline.add(self.scaler)
+        self.pipeline.add(self.filter)
+        self.pipeline.add(self.encoder)
+        self.pipeline.add(self.parser)
+        self.pipeline.add(self.sink)
 
-    print("Parsing JSON")
-    return json.loads(req.text)
+        self.source.link(self.overlay)
+        self.overlay.link(self.scaler)
+        self.scaler.link(self.filter)
+        self.filter.link(self.encoder)
+        self.encoder.link(self.parser)
+        self.parser.link(self.sink)
 
+        bus = self.pipeline.get_bus()
+        bus.add_signal_watch()
+        bus.connect("message", self.on_message)
 
-tvc_hash={}
-def clean_name(name):
-    global tvc_hash
-    out=""
-    for x in name.upper():
-        if x>='A' and x<='Z':
-            out=out+x
-        if x>='0' and x<='9':
-            out=out+x
-        if x==' ':
-            out=out+'_'
-    if len(out)<2:
-        out="INVALID"
-    if out in tvc_hash:
-        tvc_hash[out]=tvc_hash[out]+1
-        out=out+"-"+str(tvc_hash[out])
-    else:
-        tvc_hash[out]=1
-    return out
+        self.pipeline.set_state(Gst.State.PLAYING)
 
-class TVChannel:
-    def __init__(self, name, tags, number, tvh_uuid):
-        self.name=name
-        self.tags=tags
-        self.number=number
-        self.tvh_uuid=tvh_uuid
-        self.hls_uuid=clean_name(name)
-        self.tvh_url=tvh_base_url_auth+"stream/channel/"+tvh_uuid
-        self.m3u8_file=config["hls_local_path"]+"/"+self.hls_uuid+".m3u8"
-        self.stream=None
-        self.last_used=time.time()
-        self.clean_stream()
-    def start_stream(self):
-        self.last_used=time.time()
-        if self.stream:
-            if os.path.isfile(self.m3u8_file):
-                return "stream.m3u8?uuid="+self.hls_uuid
-            else:
-                if self.stream.poll() is None:
-                    return False
-        #Start stream
-        self.stream=subprocess.Popen(["/usr/bin/ffmpeg", "-i", self.tvh_url, "-probesize", "100000",
-            "-f", "hls", "-g", "50", 
-            "-preset", "fast", 
-            "-c:v", "libx264", "-b:v", "2M", 
-            "-c:a", "aac", "-b:a", "96k",
-            "-filter:v", "scale=720:576",
-            "-r", "25", "-sn",
-            "-hls_flags", "delete_segments",
-            "-hls_list_size", "100",
-            "-hls_time", "2", "-hls_playlist_type", "event",self.m3u8_file], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-        self.last_used=time.time()
-        return False
-    def clean_stream(self):
-        stream_path_base=config["hls_local_path"]
-        files=os.listdir(stream_path_base)
-        for f in files:
-            if f.startswith(self.hls_uuid):
-                print("clean_stream: erasing file %s" % (f))
-                os.remove(stream_path_base+"/"+f)
-        self.stream=None
-        return True
+    def set_channel(self, channel):
+        self.overlay.set_property("text", channel)
 
-
-def tvhedend_get_tv_channellist():
-    #Get Data from tvheadend like channels and channel tags
-    channeltags_list=tvheadend_get(tvh_base_url+"/api/channeltag/list")
-    channeltags=channeltags_list["entries"]
-    channels_grid=tvheadend_get(tvh_base_url+"/api/channel/grid?limit=99999")
-    channels=channels_grid["entries"]
-    #Get the tags for Radio and TV channels
-    radio_tag=None
-    tv_tag=None
-    channel_tags={}
-    for tag in channeltags:
-        if tag["val"]=="Radio channels":
-            radio_tag=tag["key"]
-            print("%s = %s" % (tag["key"], tag["val"]))
-        if tag["val"]=="TV channels":
-            tv_tag=tag["key"]
-            print("%s = %s" % (tag["key"], tag["val"]))
-        channel_tags[tag["key"]]=tag["val"]
-    #Filter channels to only have TV channels
-    channel_list=[]
-    for channel in channels:
-        name=channel["name"]
-        if name == "{name-not-set}":
-            continue
-        if "tags" in channel and len(channel["tags"]):
-            tag_list=[]
-            for t in channel["tags"]:
-                if t in channel_tags:
-                    if t!=tv_tag:
-                        tag_list.append(channel_tags[t])
-            tags="("+", ".join(tag_list)+")"
-        channel["new_name"]=name
-        if radio_tag in channel["tags"]:
-            continue
-        if not tv_tag is None and not tv_tag in channel["tags"]:
-            continue
-        ch=TVChannel(name, tags, channel["number"], channel["uuid"])
-        channel_list.append(ch)
-    channel_hash={}
-    for ch in channel_list:
-        channel_hash[ch.hls_uuid]=ch
-    return (sorted(channel_list, key=lambda x: getattr(x, config["sort"])), channel_hash)
-
-end_program=0
-main_thread={}
-
-
-print("Getting service list from tvheadend")
-
-channel_list=[]
-channel_hash={}
-
-(channel_list, channel_hash) = tvhedend_get_tv_channellist()
-
-        
-print("%s channels for TV services" % len(channel_list))
-
-app = FastAPI()
-
-@app.get("/")
-async def read_root(s: str="", d: str="i"):
-    if not s in ("name", "number"):
-        s=config["sort"]
-    cl_sorted=sorted(channel_list, key=lambda x: getattr(x, s), reverse=(d=='d'))
-    data="<html><head><title>Channels</title></head>"
-    data=data+"<body>"
-    data=data+"<table>"
-    data=data+'<tr><th scope="col"><a href="?s=number">▲</a><a href="?s=number&d=d">▼</a></th><th scope="col">Name <a href="?s=namer">▲</a><a href="?s=name&d=d">▼</a></th></th><th scope="col">Tags</th> </tr>'
-    for service in cl_sorted:
-        name=service.name
-        tags=service.tags
-        uuid=service.hls_uuid
-        number=str(service.number)
-        if number=="0":
-            number=""
-        data=data+'<tr><td>'+html.escape(number)+'</td><td><a href="stream?uuid='+html.escape(uuid)+'" rel="nofollow">'+html.escape(name)+'</a></td><td>'+html.escape(tags)+'</td></tr>'
-    data=data+"</table>"
-    data=data+"</body></html>"
-    return Response(content=data, media_type="text/html;charset=utf-8")
-
-@app.get("/stream.m3u8")
-async def read_m3u8(uuid: str=""):
-    if not uuid in channel_hash:
-        return Response(content="NIX", media_type="text/plain;charset=utf-8")
-    channel=channel_hash[uuid]
-    res=channel.start_stream()
-    if (res==False):
-        return Response(content="NIX", media_type="text/plain;charset=utf-8")
-
-    data=""
-    m3u8=open(channel.m3u8_file , "r")
-    for line in m3u8.readlines():
-        if line[0]=="#":
-            data=data+line
-        else:
-            data=data+config["hls_http_path"]+line
-
-    return Response(content=data, media_type="text/plain;charset=utf-8")
-
-
-def player_page(uri: str="", name: str=""):
-    data="<html><head><title>%s</title></head>"%html.escape(name)
-    data=data+"<body>"
-    data=data+'<script src="'+config["static_http_path"]+'hls.js"></script>'
-    data=data+'''
-    <center>
-      <h1>%s</h1>
-      <video width="100%%" id="video" controls></video>
-    </center>
-
-    <script>
-      var video = document.getElementById('video');
-      if (Hls.isSupported()) {
-        var hls = new Hls({
-          debug: true,
-        });
-        hls.loadSource('%s');
-        hls.attachMedia(video);
-        hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-          video.muted=true;
-          video.play();
-        });
-      }
-      else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-        video.src = '%s';
-        video.addEventListener('canplay', function () {
-          video.muted=true;
-          video.play();
-        });
-      }
-    </script>
-''' % (html.escape(name),uri,uri)
-    data=data+'<br><a href="%s">URL for use with VLC</a>'%(uri)
-    data=data+"</body>"
-
-    return Response(content=data, media_type="text/html;charset=utf-8")
-
-
-@app.get("/stream")
-async def read_stream(uuid: str=""):
-    if not uuid in channel_hash:
-        return Response(content="NIX", media_type="text/plain;charset=utf-8")
-    channel=channel_hash[uuid]
-    res=channel.start_stream()
-    if res:
-        return player_page(res, channel.name)
-    data='<html><head><title>Bitte warten</title><meta http-equiv="refresh" content="1"></head>'
-    data=data+"<body>"
-    data=data+"Bitte warten, Stream startet"
-    data=data+"</body></html>"
-    return Response(content=data, media_type="text/html;charset=utf-8")
-
-
-# for really static files, such as javascript, images etc
-app.mount(config["static_http_path"], StaticFiles(directory=config["static_local_path"]), name="static")
-# HLS stream files which are dynamically generated
-app.mount(config["hls_http_path"], StaticFiles(directory=config["hls_local_path"]), name="hls")
-
-
-def check_status():
-    global main_thread
-    global stream_ffmpeg
-    while main_thread.is_alive():
-        for channel in channel_list:
-            if channel.stream is None:
-                continue
-            age=time.time()-channel.last_used
-            if (age>30):
-                channel.stream.kill()
-                time.sleep(1)
-            if channel.stream.poll() is None:
-                continue
-            channel.clean_stream()
-        time.sleep(1)
+    def on_message(self, bus, message):
+        err, debug = message.parse_error()
+        print("Error: %s" % err, debug)
 
 
 def main():
-    global main_thread, end_program
-    main_thread=threading.currentThread()
-    x=threading.Thread(target=check_status, args=[])
-    x.start()
-    uvicorn.run(app, port=8888, host="0.0.0.0", log_level="info")    
+    Gst.init(None)
+    app = Streamer()
+
+    @app.get("/channel")
+    async def read_m3u8(channel: str = ""):
+        app.set_channel(channel)
+        return Response(content="OK", media_type="text/plain;charset=utf-8")
+
+    app.mount(config["hls_http_path"], StaticFiles(directory=config["hls_local_path"]), name="hls")
+    app.mount(config["static_http_path"], StaticFiles(directory=config["static_local_path"], html=True), name="static")
+
+    app.set_channel("test")
+    uvicorn.run(app, port=8888, host="0.0.0.0", log_level="info")
     print("Ending program")
-    end_program=1
+    time.sleep(1)
 
 
 if __name__ == "__main__":
